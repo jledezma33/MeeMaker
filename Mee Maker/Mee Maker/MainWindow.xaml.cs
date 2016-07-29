cefa422d5b1d82c87aca098e0b71aa7277198550
Jesus Ledezma <jledezma@student.neumont.edu>
2016-07-29 14:34:32 -0600

    Did size, movement, and color for all facial features.

--- Mee Maker/Mee Maker/MainWindow.xaml.cs
+++ Mee Maker/Mee Maker/MainWindow.xaml.cs
@@ -24,5 +24,114 @@ namespace Mee_Maker
         {
             InitializeComponent();
         }
+
+        private void Face_Button_Click(object sender, RoutedEventArgs e)
+        {
+            FaceOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Face_Beige_Click(object sender, RoutedEventArgs e)
+        {
+            Head.Fill = Brushes.Beige;
+        }
+
+        private void Face_Black_Click(object sender, RoutedEventArgs e)
+        {
+            Head.Fill = Brushes.Black;
+        }
+
+        private void Face_Brown_Click(object sender, RoutedEventArgs e)
+        {
+            Head.Fill = Brushes.Brown;
+        }
+
+        private void Eyes_Button_Click(object sender, RoutedEventArgs e)
+        {
+            EyeOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Eyes_Black_Click(object sender, RoutedEventArgs e)
+        {
+            Left_Eye.Fill = Brushes.Black;
+            Right_Eye.Fill = Brushes.Black;
+        }
+
+        private void Eyes_Blue_Click(object sender, RoutedEventArgs e)
+        {
+            Left_Eye.Fill = Brushes.Blue;
+            Right_Eye.Fill = Brushes.Blue;
+        }
+
+        private void Eyes_Brown_Click(object sender, RoutedEventArgs e)
+        {
+            Left_Eye.Fill = Brushes.Brown;
+            Right_Eye.Fill = Brushes.Brown;
+        }
+
+        private void Eyes_Green_Click(object sender, RoutedEventArgs e)
+        {
+            Left_Eye.Fill = Brushes.Green;
+            Right_Eye.Fill = Brushes.Green;
+        }
+
+        private void Nose_Button_Click(object sender, RoutedEventArgs e)
+        {
+            NoseOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Nose_Beige_Click(object sender, RoutedEventArgs e)
+        {
+            Nose.Fill = Brushes.Beige;
+        }
+
+        private void Nose_Black_Click(object sender, RoutedEventArgs e)
+        {
+            Nose.Fill = Brushes.Black;
+        }
+
+        private void Nose_Brown_Click(object sender, RoutedEventArgs e)
+        {
+            Nose.Fill = Brushes.Brown;
+        }
+
+        private void Mouth_Button__Click(object sender, RoutedEventArgs e)
+        {
+            MouthOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Mouth_Red_Click(object sender, RoutedEventArgs e)
+        {
+            Mouth.Fill = Brushes.Red;
+        }
+
+        private void Mouth_Black_Click(object sender, RoutedEventArgs e)
+        {
+            Mouth.Fill = Brushes.Black;
+        }
+
+        private void Mouth_Pink_Click(object sender, RoutedEventArgs e)
+        {
+           Mouth.Fill = Brushes.Pink;
+        }
+
+        private void Blemish_Button_Click(object sender, RoutedEventArgs e)
+        {
+            BlemishOptions.Visibility = Visibility.Visible;
+        }
+
+        private void Blemish_Red_Click(object sender, RoutedEventArgs e)
+        {
+            Blemish.Fill = Brushes.Red;
+        }
+
+        private void Blemish_Black_Click(object sender, RoutedEventArgs e)
+        {
+            Blemish.Fill = Brushes.Black;
+        }
+
+        private void Blemish_White_Click(object sender, RoutedEventArgs e)
+        {
+            Blemish.Fill = Brushes.White;
+        }
     }
 }
